9915fe2adf756fb0f4d55433867d0a2ec0bbf4f2
rfranck89 <rfranck89@charlottesvilleschools.org>
2023-10-26 13:46:02 -0400

    commented countdown 3

--- raspberry-pi/Crash_Avoidance1.py
+++ raspberry-pi/Crash_Avoidance1.py
@@ -9,5 +9,5 @@ scl_pin = board.GP27
 i2c = busio.I2C(scl_pin, sda_pin) #lets the pico know that pin 26 is the sda pin and pin 27 is the scl pin.
 mpu = adafruit_mpu6050.MPU6050(i2c)
 while True:
-    print(f'X = {mpu.acceleration[0]}, Y = {mpu.acceleration[1]}, Z = {mpu.acceleration[2]}') #prints the accelerometer's x, y, and z velocity 10 times every second
+    print(f'X = {mpu.acceleration[0]}, Y = {mpu.acceleration[1]}, Z = {mpu.acceleration[2]}') #prints the accelerometer's x, y, and z velocity 10 times per second
     time.sleep(.1)
--- raspberry-pi/countdown3.py
+++ raspberry-pi/countdown3.py
@@ -6,8 +6,8 @@ import digitalio
 
 button = digitalio.DigitalInOut(board.GP28)
 button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
-abort = 0
+button.pull = digitalio.Pull.UP # makes it so that when the button is not being pushed it outputs a TRUE value and when it is pushed it outputs a FALSE value.
+abort = 0 # this will be used to help with the spicy version of the assignment.
 
 red = digitalio.DigitalInOut(board.GP15)
 red.direction = digitalio.Direction.OUTPUT
@@ -15,25 +15,25 @@ red.direction = digitalio.Direction.OUTPUT
 green = digitalio.DigitalInOut(board.GP16)
 green.direction = digitalio.Direction.OUTPUT
 while True:
-    if button.value == False:
-        if abort == 0:
+    if button.value == False: #if the button is pushed
+        if abort == 0: # if the countdown is not active
             for x in range(11):
                 if x<10:
-                    if abort == 1 and button.value == False:
+                    if abort == 1 and button.value == False: # if the button is pushed during the countdown print ABORT
                         print('ABORT')
                         time.sleep(.1)
-                        break
+                        break # stops the loop
                     print(10-x)
                     red.value = True
                     time.sleep(.1)
                     red.value = False
                     green.value = False
                     time.sleep(.9)
-                    abort = 1
+                    abort = 1 # this is used to check if the countdown is active
                 else:
                     print('liftoff!')
                     while True:
-                        abort = 1
+                        abort = 1 # makes it so that you can't abort the countdown after it already finished
                         green.value = True
     else:
-        abort = 0
+        abort = 0 # this is just to make it so the abort value doesn't stay at one
